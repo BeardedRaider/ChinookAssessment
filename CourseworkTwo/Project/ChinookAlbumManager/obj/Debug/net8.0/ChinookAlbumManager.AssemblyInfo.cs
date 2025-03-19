d20092ae957513e36a80a281028378ef66f91b19
Shane Cummings <Shanecummings_uk@yahoo.co.uk>
2025-03-19 04:03:40 +0000

    created the main page to display track and artist, but its only showing the id of the artist currently

--- CourseworkTwo/Project/ChinookAlbumManager/obj/Debug/net8.0/ChinookAlbumManager.AssemblyInfo.cs
+++ CourseworkTwo/Project/ChinookAlbumManager/obj/Debug/net8.0/ChinookAlbumManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChinookAlbumManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b895f3588f3bf2e23cab293200a3b797e79a83b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1877e3b44a387e5fcaa4044f3542e14c2a4577e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChinookAlbumManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChinookAlbumManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
