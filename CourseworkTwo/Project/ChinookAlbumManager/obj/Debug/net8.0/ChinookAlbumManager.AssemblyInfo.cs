ac8473efdaffb73415f8b4db79d8e3938d4b8e25
Shane Cummings <shanecummings_uk@yahoo.co.uk>
2025-03-22 23:44:50 +0000

    Fixed an issue where the user could ned add a track to an existing album

--- CourseworkTwo/Project/ChinookAlbumManager/obj/Debug/net8.0/ChinookAlbumManager.AssemblyInfo.cs
+++ CourseworkTwo/Project/ChinookAlbumManager/obj/Debug/net8.0/ChinookAlbumManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChinookAlbumManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe95016bc0b6c5ec9b515ce716b74a56bc002d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3795c759cba3fdf8503e5a2643ec18a391db9008")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChinookAlbumManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChinookAlbumManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
